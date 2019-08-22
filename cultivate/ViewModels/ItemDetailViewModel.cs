ec33e7952797b4362d9b88c2e46fe735422087ea
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2019-08-22 14:45:56 -0700

    Add project files.

---
+++ cultivate/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using cultivate.Models;
+
+namespace cultivate.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
